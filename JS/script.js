0e92cb99461af8a08fd7ea30536ede227ec31160
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-22 15:02:35 +0100

    Taking of the the elements from the DOM

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,12 @@ INITIALITATIONS AND DECLARATIONS */
 
 // * INITIALITATIONS
 
+// ? TAKING FROM THE DOM
+const form = document.querySelector('form');
+const kmToTravelInputField = form.querySelector('input[name="km-to-travel"]');
+const ageInputField = form.querySelector('input[name="age"]');
+const btn = form.querySelector('.my-btn');
+
 
 
 // * DECLARATIONS
